11bac233c79111fcc1cca8a5df56a303447fedaa
gladson.coronado <gladson.coronado@gazin.com.br>
2023-04-20 14:15:52 -0300

    a

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+
+var currentModal = null;
+
+$('.modal').on('shown.bs.modal', function (e) {
+
+    currentModal = $(this);
+});
+
+$(document).on('keyup',function(e) {
+    if (currentModal && currentModal.find('.carousel').length) {
+
+        if(e.keyCode == 32) {
+
+            currentModal.carousel('next');
+
+        } else if (e.keyCode == 8) {
+
+            currentModal.carousel('prev');
+            return;
+
+        } 
+            
+    }
+});
+
+$(document).on('keyup', function(e) {
+
+
+    if(e.keyCode === 65 || e.keyCode === 67 ||e.keyCode === 72 ) {
+
+        if(currentModal != null) currentModal.modal('hide');
+
+        if (e.keyCode === 65) { 
+      
+            $('#AntesModal').modal('show');
+        } else  if (e.keyCode === 67) { 
+            $('#CausasModal').modal('show'); 
+        } else  if (e.keyCode === 72) { 
+            $('#HojeModal').modal('show'); 
+        }     
+
+    }
+
+});
+  
